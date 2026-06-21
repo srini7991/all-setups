c7e6ed1784c6aafb36bfc4a06e5c16433b02e601
srini7991 <srinubandaru7991@gmail.com>
2026-06-21 19:39:10 +0100

    java v

--- nexus.sh
+++ nexus.sh
@@ -1,6 +1,6 @@
 sudo yum update -y
 sudo yum install wget -y
-sudo yum install java-17-amazon-corretto-jmods -y
+sudo yum install java-21-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
 sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
 sudo tar -xvf nexus-3.79.1-04-linux-x86_64.tar.gz
